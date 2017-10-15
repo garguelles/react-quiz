41643938ef141af4a2f132923832ade0e891d5f0
Gerard Arguelles <gerard.arguelles@gmail.com>
2017-10-16 05:52:30 +0800

    [fix] styles

--- app/containers/QuizPage/Question.js
+++ app/containers/QuizPage/Question.js
@@ -11,13 +11,23 @@ import { RadioButton, RadioGroup } from 'react-radio-buttons';
 
 const Wrapper = styled.div`
   margin-bottom: 30px;
+  > div > div {
+    > div {
+      align-items: center;
+      > div:last-child {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0!important;
+      }
+    }
+  }
 `;
 
 class Question extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
   renderChoices() {
     const choices = this.props.choices;
-    console.log(choices[0]);
     return choices.map((c, i) => (
       <RadioButton rootColor="#aaa" key={i} value={c}>{c}</RadioButton>
     ));
@@ -33,7 +43,7 @@ class Question extends React.Component { // eslint-disable-line react/prefer-sta
     return (
       <Wrapper>
         <P margin="0 0 15px 0" size="1.6em" dangerouslySetInnerHTML={{__html: this.props.question}} />
-        <RadioGroup value="" onChange={this.onChange} style={{ fontSize: '16px' }}>
+        <RadioGroup vertical value="" onChange={this.onChange} style={{ fontSize: '16px' }}>
           {this.renderChoices()}
         </RadioGroup>
       </Wrapper>
