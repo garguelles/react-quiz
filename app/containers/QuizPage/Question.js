fe98169d0dca89d5afb60e3a632adf3022e8e3d5
Gerard Arguelles <gerard.arguelles@gmail.com>
2017-10-16 01:43:50 +0800

    render questions

--- app/containers/QuizPage/Question.js
+++ app/containers/QuizPage/Question.js
@@ -5,12 +5,25 @@
  */
 
 import React, { PureComponent, PropTypes } from 'react';
+import { RadioButton, RadioGroup } from 'react-radio-buttons';
 
 class Question extends React.Component { // eslint-disable-line react/prefer-stateless-function
+
+  renderChoices() {
+    const choices = this.props.incorrect_answers.concat([this.props.correct_answer]);
+
+    return choices.map((c, i) => (
+      <RadioButton key={i} value={c}>{c}</RadioButton>
+    ));
+  }
+
   render() {
     return (
       <div>
-        <p>{this.props.question}</p>
+        <p dangerouslySetInnerHTML={{__html: this.props.question}} />
+        <RadioGroup vertical>
+          {this.renderChoices()}
+        </RadioGroup>
       </div>
     );
   }
